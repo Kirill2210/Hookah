9ed59f9f78c5471f6c8470b72bfcadb0bf5a866e
Kirill2210 <kirill.konovalov.s3@gmail.com>
2021-09-08 17:56:30 +0500

    end interior

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
-$(function(){
+const swiper = new Swiper('.interior__swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: false,
+  slidesPerGroup: 1,
+  slidesPerView: 'auto',
+  spaceBetween: 30,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 
 });
