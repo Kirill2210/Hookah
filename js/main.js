ad0f657132fc039e37c0e9ac5d744ffb406f8596
Kirill2210 <kirill.konovalov.s3@gmail.com>
2021-09-09 13:15:19 +0500

    end menu

--- js/main.js
+++ js/main.js
@@ -13,3 +13,19 @@ const swiper = new Swiper('.interior__swiper', {
   },
 
 });
+
+const swiper2 = new Swiper('.menu__swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: false,
+  slidesPerGroup: 1,
+  slidesPerView: 3,
+  spaceBetween: 30,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
