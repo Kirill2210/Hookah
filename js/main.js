4ea1b1d9ef7b9831ce74a1ba150456105e6f8ac8
Kirill2210 <kirill.konovalov.s3@gmail.com>
2021-09-10 16:06:54 +0500

    end news

--- js/main.js
+++ js/main.js
@@ -8,8 +8,8 @@ const swiper = new Swiper('.interior__swiper', {
 
   // Navigation arrows
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: '.interior__swiper .swiper-button-next',
+    prevEl: '.interior__swiper .swiper-button-prev',
   },
 
 });
@@ -24,8 +24,24 @@ const swiper2 = new Swiper('.menu__swiper', {
 
   // Navigation arrows
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: '.menu__swiper .swiper-button-next',
+    prevEl: '.menu__swiper .swiper-button-prev',
+  },
+
+});
+
+const swiper3 = new Swiper('.news__swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: false,
+  slidesPerGroup: 1,
+  slidesPerView: 3,
+  spaceBetween: 30,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.news__swiper .swiper-button-next',
+    prevEl: '.news__swiper .swiper-button-prev',
   },
 
 });
